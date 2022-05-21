5fe2e4184ea0dfb4f9e7334db766b54fcd7da9e1
Jean <billaudjean@gmail.com>
2022-05-21 18:40:34 +0200

    red(list): get one beer in list

--- src/domain/__tests__/listBeer.spec.ts
+++ src/domain/__tests__/listBeer.spec.ts
@@ -1,11 +1,24 @@
 import { punkInMemoryClient } from "@/infrastructure/stubs/punkInMemoryClient";
 import { describe, expect, it } from "vitest";
+import { Beer } from "../entities/Beer";
+import type { punkClient } from "../ports/punkClient";
 import { getBeers } from "../useCase/getBeers";
+
+const getPunkClient = () => new punkInMemoryClient();
+const getBeersUseCase = (punkClient: punkClient) => new getBeers(punkClient);
+
 
 describe('list beers use case', () => {
     it('should list empty beers', async () => {
-        const punkClient = new punkInMemoryClient();
-        const useCase = new getBeers(punkClient);
+        const useCase = new getBeers(getPunkClient());
         expect(await useCase.execute()).toStrictEqual([]);
+    });
+
+    it('should have one beer', async () => {
+        const punkClient = new punkInMemoryClient();
+        const beer = new Beer('d6336d57-fd74-434e-a5d5-606eafbbadc1', 'beer', 'image', 'description');
+        punkClient.addBeer(beer);
+        const useCase = new getBeers(punkClient);
+        expect(await useCase.execute()).toStrictEqual([beer]);
     });
 })
--- src/domain/entities/Beer.ts
+++ src/domain/entities/Beer.ts
@@ -1,5 +1,6 @@
 export class Beer {
     constructor(
+        readonly id: string,
         readonly name:string,
         readonly image: string,
         readonly description: string,
